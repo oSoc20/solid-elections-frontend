6ad756f127d929b1db13355dae96eb15668a2b7e
nawatend <32384185+nawatend@users.noreply.github.com>
2020-07-22 22:38:04 +0200

    Updates

---
+++ precache-manifest.dc02dcc1101861872e4667c81da820a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233a24603231244719fcd02fa9b17b18",
+    "url": "/sep_frontend/index.html"
+  },
+  {
+    "revision": "ac3a14af62d1a7af10e2",
+    "url": "/sep_frontend/static/css/main.e42b0f9a.chunk.css"
+  },
+  {
+    "revision": "ee7a764d2f9ef5a776ec",
+    "url": "/sep_frontend/static/js/2.d67d656c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sep_frontend/static/js/2.d67d656c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3a14af62d1a7af10e2",
+    "url": "/sep_frontend/static/js/main.e9736b11.chunk.js"
+  },
+  {
+    "revision": "b864c360f8274e9b5592",
+    "url": "/sep_frontend/static/js/runtime-main.a4f58203.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sep_frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
